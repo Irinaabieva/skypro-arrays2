9ab267d7a28d03f247002561cd32acd46c363a57
VarzievE <edvarziev@gmail.com>
2022-11-17 00:18:17 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,22 @@
 public class Main {
-    public static void main(String[] args) {
+    static int[] arr = generateRandomArray();
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
     }
+
+    public static void main(String[] args) {
+        // Задача 1
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.println("Сумма затрат за месяц составила " + sum + " рублей");
+    }
+
 }
